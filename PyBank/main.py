e0336e9ad6b344b7ca9d7da6a573cc0e6ed7cb5c
sean galloway <sean.galloway@outlook.com>
2020-10-10 18:57:54 -0700

    updates to main.py; mostly done, just need to do the final analysis

---
+++ PyBank/main.py
@@ -0,0 +1,93 @@
+###############################################################################
+# Imports
+###############################################################################
+import os
+import sys
+import collections
+import pprint
+# import json
+import csv
+
+###############################################################################
+# Setup Globals
+###############################################################################
+gInputFile = "./Resources/budget_data.csv"
+gOutputFile = "./Analysis/budget_analysis.txt"
+
+###############################################################################
+# parse CSV File
+###############################################################################
+def parseCSV(csvFile):
+    # Open the input file
+    try:
+        reader = csv.reader(open(csvFile))
+    except IOError:
+        print("Error, could not open CSV File: " + csvFile)
+        sys.exit(1)
+    csvRecords = []
+
+    # Read the file into a bunch of records
+    try:
+        line = -1
+        for row in reader:
+            line += 1
+            if line == 0:
+                continue
+            # print("Line: " + str(line))
+            colDate = row[0]
+            colPnL = row[1]
+            rec = {"Date": colDate, "PnL": colPnL}
+            csvRecords.append(rec)
+
+    except csv.Error as e:
+        sys.exit('Error reading CSV File: ' + csvFile + " line number " +
+                 str(line) + " error: " + str(e))
+   # Return the results
+    return csvRecords
+
+###############################################################################
+# process CSV Records
+###############################################################################
+def processCSVRecords(csvRecords):
+    # Create the DCT object the Holds the Tally information
+    trackingDict = collections.OrderedDict()
+
+    # Iterate thru the records
+    for rec in csvRecords:
+        key = rec["Date"]
+        pnl = rec["PnL"]
+
+        if key not in trackingDict:
+            trackingDict[key] = pnl
+        else:
+            trackingDict[key] += pnl
+
+    # Return the results
+    return trackingDict
+
+###############################################################################
+# Summarize results and write them out
+###############################################################################
+def writeResult(trackingDict, outputFile):
+    # Initialize Variable
+    fileText = ""
+    
+    # Write the Analysis out
+    fh = open(outputFile, "w")
+    fh.write(fileText)
+    fh.close()
+
+###############################################################################
+# Main
+###############################################################################
+def main():
+    # Run the Functions here
+    csvRecords = parseCSV(gInputFile)
+    trackingDict = processCSVRecords(csvRecords)
+    writeResult(trackingDict, gOutputFile)
+
+###############################################################################
+# Run Main
+###############################################################################
+if __name__ == "__main__":
+    main()
